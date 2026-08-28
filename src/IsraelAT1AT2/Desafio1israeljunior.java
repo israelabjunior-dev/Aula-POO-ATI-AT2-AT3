754209d86f2f321c0031550efec79def60320049
Israel.A.B.Junior <israel.a.b.junior.iabj@outlook.com>
2026-08-28 14:19:26 -0300

    Initial commit

---
+++ src/IsraelAT1AT2/Desafio1israeljunior.java
@@ -0,0 +1,32 @@
+package Aula02.IsraelAT1AT2;
+
+public class Desafio1israeljunior {
+    public static void main(String[] args) {
+
+        // DESAFIO  1 //
+
+        String nome = "Israel";
+        String sobrenome = "Antunes De Barros Junior";
+
+        System.out.println("Nome completo: " + nome + " " + sobrenome);
+        System.out.println("Nome completo: " + nome.concat(sobrenome));
+
+        System.out.println(nome.equals(sobrenome));
+        System.out.println(nome.equalsIgnoreCase(sobrenome));
+        System.out.println(nome.length());
+
+        String nomeCompleto = "Israel Antunes De Barros Junior";
+        System.out.println("Nome em maiúsculas: " + nomeCompleto.toUpperCase());
+        System.out.println("Total de caracteres: " + nomeCompleto.length());
+        System.out.println("Primeiro caractere: " + nomeCompleto.charAt(0));
+
+        //DESAFIO  2  //
+
+        String frase02 =  " Java é muito legal ";
+
+
+
+
+
+    }
+}
