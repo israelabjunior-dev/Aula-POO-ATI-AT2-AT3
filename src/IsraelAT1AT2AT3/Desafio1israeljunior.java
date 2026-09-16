33321cfd509a9d478ce3684a0b7aa5c51790c3c5
Israel.A.B.Junior <israel.a.b.junior.iabj@outlook.com>
2026-09-16 14:42:35 -0300

    atualização

--- src/IsraelAT1AT2AT3/Desafio1israeljunior.java
+++ src/IsraelAT1AT2AT3/Desafio1israeljunior.java
@@ -4,9 +4,13 @@ public class Desafio1israeljunior {
     public static void main(String[] args) {
 
         // DESAFIO  1 //
+        String desafio01 =  "Desafio 1";
+        System.out.println(desafio01);
+        System.out.println();
+
 
         String nome = "Israel";
-        String sobrenome = "Antunes De Barros Junior";
+        String sobrenome = "Junior";
 
         System.out.println("Nome completo: " + nome + " " + sobrenome);
         System.out.println("Nome completo: " + nome.concat(sobrenome));
@@ -15,14 +19,43 @@ public class Desafio1israeljunior {
         System.out.println(nome.equalsIgnoreCase(sobrenome));
         System.out.println(nome.length());
 
-        String nomeCompleto = "Israel Antunes De Barros Junior";
+        String nomeCompleto = "Israel Junior";
         System.out.println("Nome em maiúsculas: " + nomeCompleto.toUpperCase());
         System.out.println("Total de caracteres: " + nomeCompleto.length());
         System.out.println("Primeiro caractere: " + nomeCompleto.charAt(0));
 
-        //DESAFIO  2  //
+        int idade = 38;
+        System.out.println("Idade: " + idade);
+        String cpf = "400.289.222-98";
+        System.out.println("CPF: " + cpf);
+        System.out.println();
 
-        String frase02 =  " Java é muito legal ";
+
+
+        //DESAFIO  2  //
+        String desafio02 =  "Desafio 2";
+        System.out.println(desafio02);
+        System.out.println();
+
+        String frase02 =  " Java é muito Legal! ";
+        frase02 = frase02.trim();
+        System.out.println(frase02);
+
+        System.out.println(frase02.toLowerCase().endsWith("legal!"));
+
+        frase02 = frase02.replace("Legal", "Incrível");
+        System.out.println(frase02);
+
+        String[] palavras = frase02.split(" ");
+        for (String palavra : palavras){
+            System.out.println(palavra);
+        }
+
+        //DESAFIO  3  //
+        String desafio03 =  "Desafio 3";
+        System.out.println(desafio03);
+        System.out.println();
+
 
 
 
