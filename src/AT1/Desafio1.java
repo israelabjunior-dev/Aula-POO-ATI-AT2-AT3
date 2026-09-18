f188b8eda6f4ff3ae941950b9228164e532fa189
Israel.A.B.Junior <israel.a.b.junior.iabj@outlook.com>
2026-09-18 02:49:50 -0300

    novas atualizacoes

--- src/AT1/Desafio1.java
+++ src/AT1/Desafio1.java
@@ -1,6 +1,6 @@
-package Aula02.IsraelAT1AT2;
+package AT1;
 
-public class Desafio1israeljunior {
+public class Desafio1 {
     public static void main(String[] args) {
 
         // DESAFIO  1 //
@@ -20,7 +20,7 @@ public class Desafio1israeljunior {
         System.out.println(nome.length());
 
         String nomeCompleto = "Israel Junior";
-        System.out.println("Nome em maiúsculas: " + nomeCompleto.toUpperCase());
+        System.out.println("Nome em maiúsculo: " + nomeCompleto.toUpperCase());
         System.out.println("Total de caracteres: " + nomeCompleto.length());
         System.out.println("Primeiro caractere: " + nomeCompleto.charAt(0));
 
@@ -30,31 +30,11 @@ public class Desafio1israeljunior {
         System.out.println("CPF: " + cpf);
         System.out.println();
 
-
-
-        //DESAFIO  2  //
-        String desafio02 =  "Desafio 2";
-        System.out.println(desafio02);
-        System.out.println();
-
-        String frase02 =  " Java é muito Legal! ";
-        frase02 = frase02.trim();
-        System.out.println(frase02);
-
-        System.out.println(frase02.toLowerCase().endsWith("legal!"));
-
-        frase02 = frase02.replace("Legal", "Incrível");
-        System.out.println(frase02);
-
-        String[] palavras = frase02.split(" ");
-        for (String palavra : palavras){
-            System.out.println(palavra);
-        }
-
         //DESAFIO  3  //
         String desafio03 =  "Desafio 3";
         System.out.println(desafio03);
         System.out.println();
+
 
 
 
---
+++ src/AT2/Desafio2.java
@@ -0,0 +1,26 @@
+package AT2;
+
+public class Desafio2 {
+    public static void main(String[] args) {
+
+        //DESAFIO  2  //
+
+        String desafio02 =  "Desafio 2";
+        System.out.println(desafio02);
+        System.out.println();
+
+        String frase02 =  " Java é muito Legal! ";
+        frase02 = frase02.trim();
+        System.out.println(frase02);
+
+        System.out.println(frase02.toLowerCase().endsWith("legal!"));
+
+        frase02 = frase02.replace("Legal", "Incrível");
+        System.out.println(frase02);
+
+        String[] palavras = frase02.split(" ");
+        for (String palavra : palavras){
+            System.out.println(palavra);
+        }
+    }
+}
